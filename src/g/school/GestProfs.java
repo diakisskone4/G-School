be42c5c8fab7234f7f1a4c4c5441d12686883ae3
fatan <fatan@gmail.com>
2023-03-09 15:05:38 +0000

    diakiss

--- src/g/school/GestProfs.java
+++ src/g/school/GestProfs.java
@@ -33,11 +33,11 @@ public class GestProfs extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 630, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 538, Short.MAX_VALUE)
         );
 
         pack();
